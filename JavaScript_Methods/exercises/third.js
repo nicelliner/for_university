33d027f81e9954c825aa25e6ce8290f22bfaf17c
Rostislav <87404154+nicelliner@users.noreply.github.com>
2022-04-01 02:43:17 +0300

    deadline 13.04 || Update third.js

--- JavaScript_Methods/exercises/third.js
+++ JavaScript_Methods/exercises/third.js
@@ -1,7 +1,7 @@
-const str = "Я?предпочитаю?вести?здоровый?образ?жизни";
+const str = "?Я?предпочитаю?вести?здоровый?образ?жизни";
 
 const splitStr = (str) => {
-    return str.split("?");
+    return str.slice(1,str.length).split(str[0]);
 }
 
 const toEnd = (str, underStr) => {
