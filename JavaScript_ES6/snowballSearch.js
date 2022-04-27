70d6405753be7e58aa069b3d357069cfc106bac2
Rostislav <roshpil.ro@gmail.com>
2022-04-27 08:56:01 +0300

    deadline 27.04 change snowballsearch

--- JavaScript_ES6/snowballSearch.js
+++ JavaScript_ES6/snowballSearch.js
@@ -2,14 +2,18 @@ names = ["Миша", "Гена", "Маша", "Олег", "Петя", "Вова",
 k = 30;
 
 const snowballSearch = (names, k, i = 0) => {
-    if (k <= names.slice(0, i + 1).length) return jump(names, k);
+    if (k <= names.slice(0, i + 1).length) return jump(names.slice(0, i + 1), k);
     return snowballSearch(names, jump(names.slice(0, i + 1), k), ++i);
 }
 
 const jump = (array, k, i = 0) => {
-    if ( k === 1) return array[i];
     if ( i === array.length ) return k;
+    if ( k === 1 ) return array[i];
     return jump(array, --k, ++i);
 }
+
+// const snowbalSearch = (names, k, resultArray = [], i = 0) => {
+//   return (resultArray.length >= k) ? resultArray[k - 1] : snowballSearch(names, k, resultArray.concat(names.slice(0, i)), ++i);
+// }
 
 console.log(snowballSearch(names, k))
